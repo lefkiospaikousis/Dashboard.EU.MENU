9e98dec6f5f5f8d01ff8cd225489041b0c9512cc
Lefkios Paikousis (WST) <lefkiospaik@gmail.com>
2021-10-27 16:00:26 +0300

    increase upload size

--- app.R
+++ app.R
@@ -3,5 +3,5 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE)
+options( "golem.app.prod" = TRUE, shiny.maxRequestSize = 100*1024^2)
 Dashboard.EU.MENU::run_app() # add parameters here (if any)
