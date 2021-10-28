742fb8644d21f8a89cb9d4417df1de2a0f1cfd5b
Lefkios Paikousis (WST) <lefkiospaik@gmail.com>
2021-10-28 19:08:37 +0300

    disable summarise messages on production

--- app.R
+++ app.R
@@ -3,5 +3,7 @@
 # Or use the blue button on top of this file
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-options( "golem.app.prod" = TRUE, shiny.maxRequestSize = 100*1024^2)
+options( "golem.app.prod" = TRUE, 
+         dplyr.summarise.inform = FALSE,
+         shiny.maxRequestSize = 100*1024^2)
 Dashboard.EU.MENU::run_app() # add parameters here (if any)
